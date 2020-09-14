b0da42d69735c202b404fd6c14ba8e6d52b0fd7d
faith <faith@hgtech.software>
2020-09-15 03:47:19 +0700

    add car to route

--- src/routes/car.ts
+++ src/routes/car.ts
@@ -2,5 +2,6 @@ import { Router } from "express";
 import Car from "../controllers/car";
 const router: Router = Router();
 router.post("/insert_log", Car.insertLog);
+router.post("/insert_history", Car.insertHistory);
 
 export default router;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,12 +2,14 @@ import { Router, response, request } from "express";
 import express from "express";
 import Partner from "./partner";
 import User from "./user";
+import Car from "./car";
 //import data
 const router: Router = Router();
 
 //use data
 router.use("/partner", Partner);
 router.use("/user", User);
+router.use("/car", Car);
 
 function testResponse(request: express.Request, response: express.Response) {
   response.status(200).send({ success: true });
